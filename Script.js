0c2a4f494e22b098f2a0f67352dba9509e66707c
Var <146992931+IfVar@users.noreply.github.com>
2023-11-17 09:54:06 -0600

    Update Script.js
    Added Separators And Now Adding My Previous Functions For On(X)

--- Script.js
+++ Script.js
@@ -1,7 +1,27 @@
 /* Thanks For Using GsLibrary */
 /* GsLibrary Script Is Currently On Version 1.0.1 */
 
+/* [Functions] - On(X) */
+    function OnClick(object, callback) {
+        object.addEventListener("click", callback);
+    }
 
-function NoRMenu(object){
-    object.addEventListener("contextmenu", (event) => {event.preventDefault()});
-}
+    function OnHover(object, callback) {
+        object.addEventListener("mouseover", callback);
+    }
+
+    function OnLeave(object, callback) {
+        object.addEventListener("mouseout", callback);
+    }
+
+    function OnDClick(object, callback) {
+        object.addEventListener("dblclick", callback);
+    }
+
+    function OnRClick(object, callback) {
+        object.addEventListener("contextmenu", callback);
+    }
+
+    function NoRMenu(object){
+        object.addEventListener("contextmenu", (event) => {event.preventDefault()});
+    }

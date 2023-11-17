43afc84e3819b5597d9678c9409efb7bfa90a86c
Var <146992931+IfVar@users.noreply.github.com>
2023-11-17 09:55:24 -0600

    Update Script.js
    Small Indenting Change

--- Script.js
+++ Script.js
@@ -1,27 +1,27 @@
-/* Thanks For Using GsLibrary */
-/* GsLibrary Script Is Currently On Version 1.0.1 */
+// Thanks For Using GsLibrary
+// GsLibrary Script Is Currently On Version 1.0.1
 
-/* [Functions] - On(X) */
-    function OnClick(object, callback) {
-        object.addEventListener("click", callback);
-    }
+// [Functions] - On(X)
+function OnClick(object, callback) {
+  object.addEventListener("click", callback);
+}
 
-    function OnHover(object, callback) {
-        object.addEventListener("mouseover", callback);
-    }
+function OnHover(object, callback) {
+  object.addEventListener("mouseover", callback);
+}
 
-    function OnLeave(object, callback) {
-        object.addEventListener("mouseout", callback);
-    }
+function OnLeave(object, callback) {
+  object.addEventListener("mouseout", callback);
+}
 
-    function OnDClick(object, callback) {
-        object.addEventListener("dblclick", callback);
-    }
+function OnDClick(object, callback) {
+  object.addEventListener("dblclick", callback);
+}
 
-    function OnRClick(object, callback) {
-        object.addEventListener("contextmenu", callback);
-    }
+function OnRClick(object, callback) {
+  object.addEventListener("contextmenu", callback);
+}
 
-    function NoRMenu(object){
-        object.addEventListener("contextmenu", (event) => {event.preventDefault()});
-    }
+function NoRMenu(object) {
+  object.addEventListener("contextmenu", (event) => { event.preventDefault(); });
+}
